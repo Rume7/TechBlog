c604b31dadb78f83771713865607e20d3f72dfec
Rume7 <electengrhume@gmail.com>
2022-08-26 05:31:58 +0100

    Added Delete Query.

--- src/main/java/com/codehacks/blog/blogpost/dao/BlogPostRepository.java
+++ src/main/java/com/codehacks/blog/blogpost/dao/BlogPostRepository.java
@@ -2,9 +2,12 @@ package com.codehacks.blog.blogpost.dao;
 
 import com.codehacks.blog.blogpost.entities.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 
+    @Query("SELECT b FROM Blog_Post b WHERE b.title = ?1")
+    public void deleteBlogPostByTitle(String title);
 }
