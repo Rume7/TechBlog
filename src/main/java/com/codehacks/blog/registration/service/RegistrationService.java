9376fd320eeda2e4e102a491196f92ee2aff679c
Rume7 <electengrhume@gmail.com>
2022-08-29 23:45:22 +0100

    Added method stubs.

--- src/main/java/com/codehacks/blog/registration/service/RegistrationService.java
+++ src/main/java/com/codehacks/blog/registration/service/RegistrationService.java
@@ -1,2 +1,21 @@
-package com.codehacks.blog.registration.service;public class RegistrationService {
+package com.codehacks.blog.registration.service;
+
+import com.codehacks.blog.registration.entities.Registration;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    public void register(Registration newUser) {
+
+    }
+
+    public Registration findUserByEmail(String email) {
+
+        return null;
+    }
+
+    public void delete(Registration user) {
+
+    }
 }
