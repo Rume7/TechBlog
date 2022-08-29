2f22630a3bbc80a321a7b52df661b67e045057db
Rume7 <electengrhume@gmail.com>
2022-08-30 00:43:52 +0100

    Refactored class name

---
+++ src/main/java/com/codehacks/blog/registration/controller/utility/MyPasswordEncoder.java
@@ -0,0 +1,21 @@
+package com.codehacks.blog.registration.controller.utility;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+
+@Configuration
+public class MyPasswordEncoder {
+
+    @Bean
+    public String encode(String password) {
+        int strength = 16;
+        BCryptPasswordEncoder bCryptPasswordEncoder =
+                new BCryptPasswordEncoder(strength, new SecureRandom());
+
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
+        return encodedPassword;
+    }
+}
--- src/main/java/com/codehacks/blog/registration/controller/utility/PasswordEncoder.java
+++
@@ -1,8 +0,0 @@
-package com.codehacks.blog.registration.controller.utility;
-
-public class PasswordEncoder {
-
-    public BCryptPassword encode(String password) {
-        new BCryptPasswordEncoder(password);
-    }
-}
