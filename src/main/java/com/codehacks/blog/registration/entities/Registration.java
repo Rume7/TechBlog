6d610d43c30d07a850603700f08c1ff40ad54440
Rume7 <electengrhume@gmail.com>
2022-08-30 00:18:12 +0100

    Added restraints annotation to fields.

--- src/main/java/com/codehacks/blog/registration/entities/Registration.java
+++ src/main/java/com/codehacks/blog/registration/entities/Registration.java
@@ -1,21 +1,35 @@
 package com.codehacks.blog.registration.entities;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "RegisteredUser")
 @Data
+@NoArgsConstructor
 public class Registration {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NonNull
     private String email;
+
+    @NonNull
+    @Column(length = 65, nullable = false)
     private String password;
     private String confirmPassword;
+
+    @NonNull
+    @Column(length = 20)
     private String firstName;
+
+    @NonNull
+    @Column(length = 20)
     private String lastName;
 
 
