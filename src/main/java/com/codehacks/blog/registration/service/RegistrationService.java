4bb10c70131548e0108d633ef815758df5c7f586
Rume7 <electengrhume@gmail.com>
2022-08-30 00:02:08 +0100

    Fixed the create and delete user.

--- src/main/java/com/codehacks/blog/registration/service/RegistrationService.java
+++ src/main/java/com/codehacks/blog/registration/service/RegistrationService.java
@@ -1,21 +1,32 @@
 package com.codehacks.blog.registration.service;
 
+import com.codehacks.blog.registration.controller.dao.RegistrationRepository;
 import com.codehacks.blog.registration.entities.Registration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
 
-    public void register(Registration newUser) {
+    @Autowired
+    private RegistrationRepository registrationRepository;
 
+    public Registration registerUser(Registration newUser) {
+        Registration registeredUser = registrationRepository.findUserByEmail(newUser.getEmail());
+        if (registeredUser == null) {
+            registrationRepository.save(newUser);
+            return newUser;
+        }
+        throw new IllegalArgumentException("The user already exists");
     }
 
-    public Registration findUserByEmail(String email) {
 
-        return null;
-    }
-
-    public void delete(Registration user) {
-
+    public boolean delete(Registration user) {
+        Registration registeredUser = registrationRepository.findUserByEmail(user.getEmail());
+        if (registeredUser != null) {
+            registrationRepository.delete(user);
+            return true;
+        }
+        return false;
     }
 }
