3f35d5bcaf20d07b60ab760d1c5c32ca95928b7b
Rume7 <electengrhume@gmail.com>
2022-08-31 23:30:06 +0100

    Added methods in Login service

---
+++ src/main/java/com/codehacks/blog/registration/controller/LoginController.java
@@ -0,0 +1,2 @@
+package com.codehacks.blog.registration.controller;public class LoginController {
+}
--- src/main/java/com/codehacks/blog/registration/service/LoginService.java
+++ src/main/java/com/codehacks/blog/registration/service/LoginService.java
@@ -1,2 +1,28 @@
-package com.codehacks.blog.registration.service;public class LoginService {
+package com.codehacks.blog.registration.service;
+
+import com.codehacks.blog.registration.dao.LoginUserRepository;
+import com.codehacks.blog.registration.entities.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private LoginUserRepository loginUserRepository;
+
+    public List<LoginUser> getAllLoginUsers() {
+        return loginUserRepository.findAll();
+    }
+
+    public LoginUser getAUserLogin(String email) {
+        return loginUserRepository.findLoginUserByEmail(email);
+    }
+
+    /* column in the form of username | salt | password. */
+    public String getUserPassword(String email) {
+        return getAUserLogin(email).getPassword();
+    }
 }
