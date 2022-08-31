a883305f5eab0f8aaebae98fd93e2c26e9496883
Rume7 <electengrhume@gmail.com>
2022-09-01 00:42:02 +0100

    Added repository and service.

--- src/main/java/com/codehacks/blog/blogpost_v2/repositories/RegistrationRepository.java
+++ src/main/java/com/codehacks/blog/blogpost_v2/repositories/RegistrationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-    
+
 }
--- src/main/java/com/codehacks/blog/blogpost_v2/services/BlogPostService.java
+++ src/main/java/com/codehacks/blog/blogpost_v2/services/BlogPostService.java
@@ -1,2 +1,10 @@
-package com.codehacks.blog.blogpost_v2.services;public class BlogPostService {
+package com.codehacks.blog.blogpost_v2.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogPostService {
+
+    // TODO : Implement all CRUD operations
+
 }
