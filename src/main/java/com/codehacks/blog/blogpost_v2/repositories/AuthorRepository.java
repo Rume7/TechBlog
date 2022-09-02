ec71241bc48490cca08cbef7fe05a74eb8a769d5
Rume7 <electengrhume@gmail.com>
2022-09-02 21:29:22 +0100

    Added query methods to repositories

--- src/main/java/com/codehacks/blog/blogpost_v2/repositories/AuthorRepository.java
+++ src/main/java/com/codehacks/blog/blogpost_v2/repositories/AuthorRepository.java
@@ -2,9 +2,14 @@ package com.codehacks.blog.blogpost_v2.repositories;
 
 import com.codehacks.blog.blogpost_v2.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    // TODO: Implement query to determine # of blog posts associated with an Author
+    @Query("SELECT COUNT(b) FROM Blogpost b WHERE b.author_id=?1")
+    public Integer getNumberOfArticlesOfAnAuthor(Author author);
 
 }
