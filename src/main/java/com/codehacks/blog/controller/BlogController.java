4d9cf67d891794fe6d444d9c406b15c08127f5b7
Rume7 <electengrhume@gmail.com>
2022-09-04 16:57:22 +0100

    Added logger field.

--- src/main/java/com/codehacks/blog/controller/BlogController.java
+++ src/main/java/com/codehacks/blog/controller/BlogController.java
@@ -1,9 +1,21 @@
 package com.codehacks.blog.controller;
 
+import com.codehacks.blog.models.BlogPost;
+import com.codehacks.blog.services.BlogPostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BlogController {
+
+    private final Logger logger = LoggerFactory.getLogger(BlogPost.class);
+
+    @Autowired
+    private BlogPostService blogPostService;
+
+    // TODO: Implemented methods
 
 
 }
