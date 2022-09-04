12205c041f91f34f9a6c229b41698871748a7984
Rume7 <electengrhume@gmail.com>
2022-09-04 14:52:15 +0100

    Changed query parameter.

--- src/main/java/com/codehacks/blog/repositories/AuthorRepository.java
+++ src/main/java/com/codehacks/blog/repositories/AuthorRepository.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    // TODO: Implement query to determine # of blog posts associated with an Author
+    
     @Query("SELECT COUNT(b) FROM Blogpost b WHERE b.author_id=?1")
-    public Integer getNumberOfArticlesOfAnAuthor(Author author);
+    public Integer getNumberOfArticlesOfAnAuthor(Long authorId);
 
 }
