b2dc72c7c40be3bc74e8b4e0c14720cc38523ad0
Rume7 <electengrhume@gmail.com>
2022-09-04 14:32:03 +0100

    Implemented the get by title query.

--- src/main/java/com/codehacks/blog/repositories/BlogPostRepository.java
+++ src/main/java/com/codehacks/blog/repositories/BlogPostRepository.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 
-    @Query("SELECT b FROM Blog_Post b WHERE b.title = ?1")
+    @Query("DELETE b FROM Blog_Post b WHERE b.title = ?1")
     public void deleteBlogPostByTitle(String title);
+
+    @Query("SELECT b FROM Blog_Post b WHERE b.title = ?1")
+    public BlogPost getBlogPostByTitle(String title) ;
 
 }
