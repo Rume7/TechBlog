6347be3d82c79a5eb46d25ebabd3c3c9adb86034
Rume7 <electengrhume@gmail.com>
2022-09-04 16:07:22 +0100

    Implemented CRUD methods in class

--- src/main/java/com/codehacks/blog/repositories/RegistrationRepository.java
+++ src/main/java/com/codehacks/blog/repositories/RegistrationRepository.java
@@ -2,9 +2,12 @@ package com.codehacks.blog.repositories;
 
 import com.codehacks.blog.models.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
+    @Query("SELECT s FROM Registration s WHERE s.email = ?1")
+    public Registration findByEmail(String email);
 }
