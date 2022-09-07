8a87ae6adad865453cccf632450e46228c1109d1
Rume7 <electengrhume@gmail.com>
2022-09-07 04:41:18 +0100

    Added Author and Comment objects as Seed Data.

--- src/main/java/com/codehacks/blog/config/BlogPostDataLoader.java
+++ src/main/java/com/codehacks/blog/config/BlogPostDataLoader.java
@@ -2,12 +2,16 @@ package com.codehacks.blog.config;
 
 import com.codehacks.blog.models.Author;
 import com.codehacks.blog.models.BlogPost;
+import com.codehacks.blog.models.Comment;
+import com.codehacks.blog.repositories.CommentRepository;
 import com.codehacks.blog.services.BlogPostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class BlogPostDataLoader implements CommandLineRunner {
@@ -17,6 +21,9 @@ public class BlogPostDataLoader implements CommandLineRunner {
     @Autowired
     private BlogPostService blogPostService;
 
+    @Autowired
+    private CommentRepository commentRepository;
+
     @Override
     public void run(String... args) throws Exception {
         loadSeedData();
@@ -24,8 +31,13 @@ public class BlogPostDataLoader implements CommandLineRunner {
 
     private void loadSeedData() {
         if (blogPostService.getAllBlogPosts().size() == 0) {
+            Comment comment1 = new Comment("Your application runs well", "Gary Smith");
+            Comment comment2 = new Comment("Your application needs a change.", "Susan Boyle");
+            List<Comment> commentList1 = List.of(comment1, comment2);
+
             BlogPost post1 = new BlogPost("Concurrency", "Learning multithreading",
                     new Author("Wale", "Thompson", "walethompson@gmail.com"));
+            post1.setComments(commentList1);
 
             BlogPost post2 = new BlogPost("Threads", "coding threads in java 6",
                     new Author("Sally", "Graham", "sally_graham@gmail.com"));
@@ -34,7 +46,10 @@ public class BlogPostDataLoader implements CommandLineRunner {
                     new Author("Barry", "White", "barry@yahoo.com"));
 
             BlogPost post4 = new BlogPost("Spring Actions", "Aspect oriented programming",
-                    new Author("Samantha", "Booke", "booke@saphire.com"));
+                    new Author("Samantha", "Booke", "booke@hotmail.com"));
+
+            commentRepository.save(comment1);
+            commentRepository.save(comment2);
 
             blogPostService.createABlogPost(post1);
             blogPostService.createABlogPost(post2);
