ec8db4ddfa9732eea56246001358afb76071b7bb
Rume7 <electengrhume@gmail.com>
2022-09-20 15:32:06 +0100

    Createdthe index method.

--- src/main/java/com/codehacks/blog/controller/BlogController.java
+++ src/main/java/com/codehacks/blog/controller/BlogController.java
@@ -5,6 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.Instant;
+import java.time.ZoneId;
 
 @Controller
 public class BlogController {
@@ -19,4 +23,12 @@ public class BlogController {
     }
 
 
+    public ModelAndView index() {
+        logger.info("Reguest to GET index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("blogPost", blogPostService.getAllBlogPosts());
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault())
+                .toLocalDate().getDayOfWeek());
+        return modelAndView;
+    }
 }
